8114e0deb65a31107ad58527b537e740363b2f6b
msolanoimagina <msolano@imaginacolombia.com>
2018-11-19 14:34:40 -0500

    +Add: versión 0.0.4 +Add: services INDEX

--- _services/slider.js
+++ _services/slider.js
@@ -3,13 +3,32 @@ import config from 'src/config/index'
 import http from "axios"
 
 export default {
+
+  /**
+   * Get Categories by parameters
+   */
+  index(parameters) {
+    parameters ? true : parameters = []
+
+    return new Promise((resolve, reject) => {
+      http.get(config('api.api_url') + '/slide',{params: parameters})
+        .then(response => {
+          resolve(response.data);
+        })
+        .catch(error => {
+          reject([]);
+        })
+    });
+  },
+
+
   /**
    * Get Slider by ID
    * @param ID
    */
   show(id){
     return new Promise((resolve, reject) => {
-      http.get(config('api.api_url') + '/slider/'+id)
+      http.get(config('api.api_url') + '/slide/'+id)
         .then(response => {
           resolve(response.data);
         })
