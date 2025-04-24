65c687de5ff7d9da403cd68df3aa3a97f5c9e0f3
msolanorenuity <msolano@contractor.renuityhome.com>
2025-04-24 16:00:30 -0500

    added locatables fields to Slide CRUD

--- _crud/slides.vue
+++ _crud/slides.vue
@@ -3,14 +3,14 @@
 export default {
   data() {
     return {
-      crudId: this.$uid()
-    }
+      crudId: this.$uid(),
+    };
   },
   computed: {
     crudData() {
       return {
         crudId: this.crudId,
-        entityName: config("main.qslider.entityNames.slide"),
+        entityName: config('main.qslider.entityNames.slide'),
         apiRoute: 'apiRoutes.qslider.slides',
         permission: 'slider.slides',
         extraFormFields: 'Slider.crud-fields.slides',
@@ -19,28 +19,29 @@ export default {
         },
         read: {
           columns: [],
-          requestParams: {}
+          requestParams: {},
         },
         update: {
           title: this.$tr('slider.cms.updateSlide'),
+          requestParams: { include: 'locatable' },
         },
         delete: true,
         formLeft: {
-          id: {value: ''},
-          userId: {value: this.$store.state.quserAuth.userId},
+          id: { value: '' },
+          userId: { value: this.$store.state.quserAuth.userId },
           title: {
             value: '',
             type: 'input',
             isTranslatable: true,
             props: {
-              label: `${this.$tr('isite.cms.form.title')}`
+              label: `${this.$tr('isite.cms.form.title')}`,
             },
           },
           caption: {
             value: '',
             type: 'input',
             isTranslatable: true,
-            props: {label: this.$tr('slider.cms.label.buttonText')},
+            props: { label: this.$tr('slider.cms.label.buttonText') },
           },
           uri: {
             value: '',
@@ -73,7 +74,7 @@ export default {
             props: {
               type: 'textarea',
               label: `${this.$tr('isite.cms.form.summary')}`,
-            }
+            },
           },
           customHtml: {
             value: '',
@@ -81,7 +82,7 @@ export default {
             isTranslatable: true,
             props: {
               label: `${this.$tr('isite.cms.form.description')}`,
-            }
+            },
           },
         },
         formRight: {
@@ -92,10 +93,10 @@ export default {
             props: {
               label: this.$tr('isite.cms.form.status'),
               options: [
-                {label: this.$tr('isite.cms.label.enabled'), value: '1'},
-                {label: this.$tr('isite.cms.label.disabled'), value: '0'},
-              ]
-            }
+                { label: this.$tr('isite.cms.label.enabled'), value: '1' },
+                { label: this.$tr('isite.cms.label.disabled'), value: '0' },
+              ],
+            },
           },
           targetField: {
             name: 'target',
@@ -104,10 +105,10 @@ export default {
             props: {
               label: this.$tr('isite.cms.form.option'),
               options: [
-                {label: 'Same tab', value: '_self'},
-                {label: 'New tab', value: '_blank'}
-              ]
-            }
+                { label: 'Same tab', value: '_self' },
+                { label: 'New tab', value: '_blank' },
+              ],
+            },
           },
           typeField: {
             name: 'type',
@@ -116,12 +117,12 @@ export default {
             props: {
               label: this.$tr('isite.cms.form.type'),
               options: [
-                {label: 'Auto', value: 'auto'},
-                {label: '360', value: '360'},
-                {label: 'Video', value: 'video'},
-                {label: 'Image', value: 'image'}
-              ]
-            }
+                { label: 'Auto', value: 'auto' },
+                { label: '360', value: '360' },
+                { label: 'Video', value: 'video' },
+                { label: 'Image', value: 'image' },
+              ],
+            },
           },
           responsive: {
             value: '1',
@@ -129,11 +130,63 @@ export default {
             props: {
               label: this.$tr('isite.cms.label.responsive'),
               options: [
-                {label: `${this.$tr('isite.cms.label.mobile')} & PC`, value: '1'},
-                {label: "PC", value: '2'},
-                {label: this.$tr('isite.cms.label.mobile'), value: '3'},
-              ]
-            }
+                {
+                  label: `${this.$tr('isite.cms.label.mobile')} & PC`,
+                  value: '1',
+                },
+                { label: 'PC', value: '2' },
+                { label: this.$tr('isite.cms.label.mobile'), value: '3' },
+              ],
+            },
+          },
+          countryId: {
+            value: null,
+            type: 'select',
+            fakeFieldName: 'locatable',
+            props: {
+              label: this.$tr('isite.cms.label.country'),
+              clearable: true,
+            },
+            loadOptions: {
+              apiRoute: 'apiRoutes.qlocations.countries',
+              select: { label: 'name', id: 'id' },
+            },
+          },
+          provinceId: {
+            value: null,
+            type: 'select',
+            fakeFieldName: 'locatable',
+            props: {
+              label: this.$tr('isite.cms.label.department'),
+              readonly: this.crudInfo.locatable?.countryId ? false : true,
+              clearable: true,
+            },
+            loadOptions: {
+              apiRoute: this.crudInfo.locatable?.countryId
+                ? 'apiRoutes.qlocations.provinces'
+                : false,
+              select: { label: 'name', id: 'id' },
+              requestParams: { filter: { country: this.crudInfo.locatable?.countryId } },
+            },
+          },
+          cityId: {
+            value: null,
+            type: 'select',
+            fakeFieldName: 'locatable',
+            props: {
+              label: this.$tr('isite.cms.form.city'),
+              readonly: this.crudInfo.locatable?.provinceId ? false : true,
+              clearable: true,
+            },
+            loadOptions: {
+              apiRoute: this.crudInfo.locatable?.provinceId
+                ? 'apiRoutes.qlocations.cities'
+                : false,
+              select: { label: 'name', id: 'id' },
+              requestParams: {
+                filter: { province_id: this.crudInfo.locatable?.provinceId },
+              },
+            },
           },
           mediasSingle: {
             value: {},
@@ -141,17 +194,17 @@ export default {
             props: {
               label: this.$tr('isite.cms.form.image'),
               zone: 'slideimage',
-              entity: "Modules\\Slider\\Entities\\Slide",
-              entityId: null
-            }
-          }
+              entity: 'Modules\\Slider\\Entities\\Slide',
+              entityId: null,
+            },
+          },
         },
-      }
+      };
     },
     //Crud info
     crudInfo() {
-      return this.$store.state.qcrudComponent.component[this.crudId] || {}
-    }
+      return this.$store.state.qcrudComponent.component[this.crudId] || {};
+    },
   },
-}
+};
 </script>
